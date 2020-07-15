d43ec8c1c87897fab645457d63f4bd28668ce6ba
sundown <no>
2020-07-15 16:03:45 +1200

    portability
    readme
    
    licence
    
    fix padding

---
+++ README.md
@@ -0,0 +1,17 @@
+Short program to **sort strings** (stdin) **by weight** (sum of chars in string).
+
+This is **completely useless** but serves as a simple showcase of C including its *typedefs, structs, macros, loops, and syntax abuse.*
+
+
+How it works:
+```
+$ wsort XYZ abc ABCD ... 123
+
+... 	[46 46 46]	->	138
+123 	[49 50 51]	->	150
+ABCD 	[65 66 67 68]	->	266
+XYZ 	[88 89 90]	->	267
+abc 	[97 98 99]	->	294
+```
+
+**PRs welcome** and please include comments. :monkey:
--- wsort.c
+++ wsort.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <strings.h>
 
 #define MAX 8192
 
-typedef unsigned int ui;
-typedef long int li;
-
 typedef struct __attribute__((__packed__)) {
   char* str;
-  ui    sum;
+  uint32_t sum;
 } Entry;
 
 Entry list[MAX] = { { 0 } };
 
 // Messy swap macros to be used in sorting
 // Swap string
-#define SSTR(a, b, c) (list[a].str = ((char*)((li)list[b].str ^ (li)list[c].str)))
+#define SSTR(a, b, c) (list[a].str = ((char*)((int64_t)list[b].str ^ (int64_t)list[c].str)))
 // Swap int
 #define SINT(a, b, c) (list[a].sum = (list[b].sum ^ list[c].sum))
 
-ui strsum(char* str) {
-    ui t = 0;
+uint32_t strsum(char* str) {
+  uint32_t t = 0;
 
-    // Iterate through str char-by-char and add int value of chars
-    for (ui i = 0; i <= strlen(str) + 1 && str[i] != '\0'; t += (ui)str[i], i++);
-    return t;
+  // Iterate through str char-by-char and add int value of chars
+  for (uint32_t i = 0; i <= strlen(str) + 1 && str[i] != '\0'; t += (uint32_t)str[i], i++);
+  return t;
 }
 
 int main(int argc, char** argv) {
@@ -35,14 +33,14 @@ int main(int argc, char** argv) {
   
   // Iterate through args and call strsum() on each arg
   // Add sum of string to list along with string pointer
-  for (ui s = 1; argv[s] != NULL; s++) {
+  for (uint32_t s = 1; argv[s] != NULL; s++) {
     list[s - 1].sum = strsum(argv[s]);
     list[s - 1].str = argv[s];
   }
 
   // Yucky sort
-  for (ui i = 0; i < MAX && list[i].str != NULL; i++) {
-    for (ui j = 0; j < MAX && list[j].str != NULL; j++) {
+  for (uint32_t i = 0; i < MAX && list[i].str != NULL; i++) {
+    for (uint32_t j = 0; j < MAX && list[j].str != NULL; j++) {
       if (list[j].sum > list[i].sum) {
         SINT(i, i, j);
         SINT(j, i, j);
@@ -54,7 +52,7 @@ int main(int argc, char** argv) {
     }
   }
 
-  for (ui c = 0; c < MAX && list[c].str != NULL; printf("%s\n", list[c].str), c++);
+  for (uint32_t c = 0; c < MAX && list[c].str != NULL; printf("%s\n", list[c].str), c++);
 
   return 0;
 }
